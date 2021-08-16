946dbd4c96e29ec6a0dadc8f322b3350bd471b9b
Jackson Kwok <jkwok@svi.edu.au>
2021-08-16 19:00:31 +1000

    Add controllers

--- animate/R/rmarkdown.R
+++ animate/R/rmarkdown.R
@@ -1,10 +1,13 @@
 #' Insert an animated plot into an R Markdown document
 #'
 #' @param file The exported plot.
-# #' @param ... Optional arguments to pass to the `iframe` call from `shiny::tags`.
+#' @param options A character string; the JavaScript to customise the playback options.
+#' Two basic options `click_to_play()` and `click_to_loop()` have been implemented
+#' for general usage.
+#' @param style Optional style for the iframe that hosts the visualisation.
 #'
 #' @export
-insert_animate <- function(file) {
+insert_animate <- function(file, options = click_to_play(), style) {
   dir0 <- tempdir()
   asset <- function(x) system.file(x, package = "animate")
   script <- function(x) as.character(to_shiny_tag(x))
@@ -12,9 +15,11 @@ insert_animate <- function(file) {
   # args <- list(...)
   # if (!"style" %in% names(args)) {
   device <- jsonlite::fromJSON(file)$device
-  style <- paste0("border: none;",
-                  sprintf("width: %spx;", device$width + 50),
-                  sprintf("height: %spx;", device$height + 50))
+  if (missing(style)) {
+    style <- paste0("border: none;",
+                    sprintf("width: %spx;", device$width + 50),
+                    sprintf("height: %spx;", device$height + 50))
+  }
     # args$style <- style
   # }
 
@@ -26,18 +31,13 @@ insert_animate <- function(file) {
   init_str <- paste(
     "JS_device = new plot2(-1);",
     sprintf("JS_device.import(%s);", object_name),
-    "ctrl = new controller(JS_device);",
-    "ctrl.loop();",
+    options,
     sep = "\n"
   )
 
   # Create html app
   html_str <- paste(
     "<!DOCTYPE html><html><head></head><body>",
-    script(asset("assets/d3.v5.min.js")),
-    script(asset("assets/ramda.min.js")),
-    script(asset("assets/broadcast.js")),
-    script(asset("assets/d3-symbol-extra.min.js")),
     script(asset("dist/animate.js")),
     sprintf("<script>%s</script>", data_str),
     sprintf("<script>%s</script>", init_str),
@@ -47,6 +47,54 @@ insert_animate <- function(file) {
 
   shiny::tags$iframe(srcdoc = html_str, style = style)
 }
+
+#' Click an element to play a frame
+#' @param selector The ID of the DOM element.
+#' @param start An integer; the number of frames to execute upon the beginning
+#' of the visualisation. This is useful when one wants to start with some set-up
+#' instead of an empty canvas.
+#' @export
+click_to_play <- function(selector = "#SVG_1", start = 2) {
+  sprintf(
+    'ctrl = new controller(JS_device);
+    (async () => {
+      await ctrl.play_until(%s, 1);
+    })().then(() => {
+      ctrl.watch("%s", "click", function() { ctrl.play() });
+    })',
+    start, selector
+  )
+}
+
+#' Click an element to play all frames
+#' @param selector The ID of the DOM element.
+#' @param start An integer; the number of frames to execute upon the beginning
+#' of the visualisation. This is useful when one wants to start with some set-up
+#' instead of an empty canvas.
+#' @param wait A number; the number of milliseconds to wait for before the
+#' next frame is drawn.
+#' @export
+click_to_loop <- function(selector = "#SVG_1", start = 2, wait = 20) {
+  sprintf(
+    'ctrl = new controller(JS_device);
+    ctrl.play_until(%s, 1);
+    ctrl.watch("%s", "click", function() { ctrl.play_until(0, %s) });',
+    start, selector, wait
+  )
+}
+
+#' #' Loop through the available frames n times
+#' #' @param times An integer; the number of times to loop.
+#' #' @param wait A number; the number of milliseconds to wait for before the
+#' #' next frame is drawn.
+#' #' @export
+#' loop <- function(times = 1, wait = 20) {
+#'   sprintf(
+#'     "ctrl = new controller(JS_device);
+#'     ctrl.loop(%s, %s);",
+#'     times, wait
+#'   )
+#' }
 
 
 # Convert JSON to JS ----------------------------------------------------------
--- animate/inst/src/addons/controller.R
+++ animate/inst/src/addons/controller.R
@@ -3,39 +3,150 @@
 controller <- R6Class(
   "controller",
   public = list(
+    #' @field plots A 'plot2'-class object
     plots = NULL,
     player_pointer = -1,
     player_handle = NULL,  # needed to handle loop
 
     initialize = function(p) {
       self$plots <- p
-      self$player_pointer <- ifelse(p$length() > 0, 0, -1)
+
+      num_cmd <- self$plots$get_max_num_commands()
+      self$player_pointer <- ifelse(num_cmd > 0 || num_cmd == -1, 0, -1)
+
       self
     },
 
+    #' @description Play the next frame
+    #' @note If the last frame is reached, play the first frame.
     play = function() {
-      p_obj <- self$plots
-      ind <- self$player_pointer
+      p_obj   <- self$plots
+      num_cmd <- self$plots$get_max_num_commands()
+      ind     <- self$player_pointer
       if (ind >= 0) {
         p_obj$dispatch_by_idx(ind)
-        self$player_pointer <- (ind + 1) %% p_obj$length()
+        self$player_pointer <- (ind + 1) %% num_cmd
       }
       TRUE
     },
 
-    loop = function() {
-      p_obj <- self$plots
-      if (!self$player_handle && self$player_pointer >= 0) {
-        self$player_handle <- setInterval(function() {
-          p_obj$dispatch_by_idx(self$player_pointer)
-          self$player_pointer <- (self$player_pointer + 1) %% p_obj$length()
-          TRUE
-        }, 300)
+    #' @description Play the next n frames
+    #' @param n_frames An integer.
+    #' @param wait A number; the number of milliseconds to wait for before the
+    #' next frame is drawn.
+    play_for = function(n_frames, wait = 20) {
+      if (self$player_pointer < 0) {
+        return(NULL)
       }
+      play_n_frame <- function() {
+        frame_counter <- 0
+        function(handle) {
+          self$play()
+          frame_counter <<- frame_counter + 1
+          if (frame_counter >= n_frames) {
+            window$cancelAnimationFrame(handle)
+          }
+          TRUE
+        }
+      }
+      self$player_handle <- make_animation(play_n_frame(), wait)()
       TRUE
+    },
+
+    #' @description Play until a certain frame is reached
+    #' @param frame_num An integer; the target frame number.
+    #' @param wait A number; the number of milliseconds to wait for before the
+    #' next frame is drawn.
+    #' @note The playing of the frames is exclusive, i.e., the function stops
+    #' right before the target frame is reached, and the target frame is not
+    #' executed. To loop through the entire commands stack, use 0 for the `frame_num`.
+    play_until = function(frame_num, wait = 20) {
+      if (self$player_pointer < 0) {
+        return(NULL)
+      }
+      play_until_frame <- function() {
+        function(handle) {
+          self$play()
+          if (self$player_pointer == frame_num) {
+            window$cancelAnimationFrame(handle)
+          }
+          TRUE
+        }
+      }
+      self$player_handle <- make_animation(play_until_frame(), wait)()
+      TRUE
+    },
+
+    #' @description Loop through the available frames n times
+    #' @param times An integer; the number of times to loop.
+    #' @param wait A number; the number of milliseconds to wait for before the
+    #' next frame is drawn.
+    loop = function(times = 1, wait = 20) {
+      if (self$player_pointer < 0) {
+        return(NULL)
+      }
+      loop_through <- function() {
+        counter <- 0
+        function(handle) {
+          self$play()
+          if (self$player_pointer == 0) {
+            counter <<- counter + 1
+          }
+          if (counter >= times) {
+            window$cancelAnimationFrame(handle)
+          }
+          TRUE
+        }
+      }
+      self$player_handle <- make_animation(loop_through(), wait)()
+      TRUE
+    },
+
+    #' @description Execute a callback function when a event is triggered.
+    #' @param selector A character string; the query selector.
+    #' @param event A character string; a HTML DOM event.
+    #' @param callback A function; the callback function.
+    watch = function(selector, event, callback) {
+      select_dom(selector)$addEventListener(event, callback)
     }
   ),
   private = list()
 )
 
-# ctrl <- controller$new(JS_device)
+
+#' Execute a function every s milliseconds
+#'
+#' @param f A function; the first argument is the handle, which can be used to
+#' stop the animation when an ending condition is reached.
+#' @param wait The milliseconds.
+#'
+#' @examples
+#' f <- function() {
+#'   counter <- 0
+#'   function(handle) {
+#'     counter <<- counter + 1
+#'     console::log(counter)
+#'     if (counter >= 20) {
+#'       window$cancelAnimationFrame(handle)
+#'     }
+#'   }
+#' }
+#' timer_1 <- make_animation(f(), 200)
+#' res <- timer_1()
+#'
+#' @export
+make_animation <- function(f, wait) {
+  then <- performance$now()
+  main <- function() {
+    timer <- requestAnimationFrame(main)
+    now <- performance$now()
+    elapsed <- now - then
+    if (elapsed > wait) {
+      then <<- now - (elapsed %% wait)
+      # main animation loop
+      f(timer)
+    }
+    return(timer)
+  }
+  main
+}
